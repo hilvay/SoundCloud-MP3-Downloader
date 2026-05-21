b8e5b0e9c97d7dd86fcc9d719bb0e2a0c7b4542a
hilvay <bajdakovmaksim7@gmail.com>
2026-05-21 19:52:47 +0300

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,34 @@
+@echo off
+chcp 65001 > nul
+
+if not exist "yt-dlp.exe" (
+    echo [!] Локальный yt-dlp.exe не найден. Качаю рабочую версию...
+    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe' -OutFile 'yt-dlp.exe'"
+    cls
+)
+
+:loop
+cls
+echo ================================
+echo     УНИВЕРСАЛЬНЫЙ УСТАНОВЩИК 
+echo ================================
+echo.
+
+set /p url="Вставь ссылку на ТРЕК или ПЛЕЙЛИСТ и нажми Enter: "
+
+echo.
+echo [!] Начинаю скачивание...
+echo.
+
+"yt-dlp.exe" -x --audio-format mp3 --audio-quality 0 --add-metadata --embed-thumbnail --yes-playlist -o "%%(playlist_title,playlist)s/%%(title)s.%%(ext)s" "%url%"
+
+echo.
+echo [+] Готово! Всё разложено по папкам плейлистов.
+echo =======================================================
+echo.
+
+set /p choice="Хочешь скачать что-то ещё? (Y - да, N - закрыть): "
+if /i "%choice%"=="Y" goto loop
+if /i "%choice%"=="Н" goto loop
+
+exit
